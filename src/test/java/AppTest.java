bfbffb3a8e95c4db58ffdcc9cd741ec8c2366475
Bharathraj <bharathraj.r130723@email.com>
2026-06-04 18:40:03 +0530

    Updated value

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,6 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     void testAdd() {
-        assertEquals(5, App.add(2,3));
+        assertEquals(100, App.add(2,3));
     }
 }
